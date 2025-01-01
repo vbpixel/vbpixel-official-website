8466921dc9a4a1f266bc0a9bb208033fa2bd9876
ASKLL-STAR <askll520@outlook.com>
2025-01-01 21:15:46 +0800

    Update index.js

--- static/javascripts/index.js
+++ static/javascripts/index.js
@@ -1,16 +1,23 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const serverStatusElement = document.getElementById('player-list');
-  
-    try {
-      const response = await fetch('/.netlify/functions/getPlayerList');
-      const data = await response.json();
-  
-      if (response.ok) {
-        serverStatusElement.innerHTML = `<p>${data.message}</p>`;
-      } else {
-        serverStatusElement.innerHTML = `<p>服务器貌似离线了( 可能是在维护吧</p>`;
-      }
-    } catch (error) {
-      serverStatusElement.innerHTML = `<p>服务器貌似离线了( 可能是在维护吧</p>``<p>错误信息:${error.message}</p>`;
-    }
-  });
+function fetchPlayerList(){
+    document.addEventListener('DOMContentLoaded', async () => {
+        const serverStatusElement = document.getElementById('player-list');
+    
+        try {
+        const response = await fetch('/.netlify/functions/getPlayerList');
+        const data = await response.json();
+    
+        if (response.ok) {
+            serverStatusElement.innerHTML = `<p>${data.message}</p>`;
+        } else {
+            serverStatusElement.innerHTML = `<p>服务器貌似离线了(</p>`;
+        }
+        } catch (error) {
+        serverStatusElement.innerHTML = `<p>糟了！</p>``<p>错误信息:${error.message}</p>`;
+        }
+    });
+}
+
+// 页面加载完成后获取在线状态
+$(document).ready(function() {
+    fetchPlayerList();
+});
