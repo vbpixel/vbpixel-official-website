c7a91640907b3dba3bd3816008f0f1c3f35ae78e
ASKLL-STAR <askll520@outlook.com>
2025-01-01 20:48:22 +0800

    update

--- static/javascripts/index.js
+++ static/javascripts/index.js
@@ -1,25 +1,16 @@
-function fetchOnlinePlayers() {
-    const serverIp = 'mc.vbpixel.cn';
-    const apiUrl = `https://list.mczfw.cn/api/${serverIp}`;
-
-    $.getJSON(apiUrl, function(data) {
-        if (data.p !== undefined) {
-            const playersOnline = data.p;
-            const maxPlayers = data.mp;
-            if (data.ping != null){
-                $('#server-status').html(`服务器状态：在线 | 当前人数：${playersOnline}/${maxPlayers}`);
-            }else {
-                $('#server-status').html(`服务器状态：离线`);
-            }
-        } else {
-            $('#server-status').html('无法获取服务器状态');
-        }
-    }).fail(function() {
-        $('#server-status').html('无法连接到服务器');
-    });
-}
-
-// 页面加载完成后获取在线状态
-$(document).ready(function() {
-    fetchOnlinePlayers();
-});
+document.addEventListener('DOMContentLoaded', async () => {
+    const serverStatusElement = document.getElementById('player-list');
+  
+    try {
+      const response = await fetch('/.netlify/functions/getPlayerList');
+      const data = await response.json();
+  
+      if (response.ok) {
+        serverStatusElement.innerHTML = `<p>${data.message}</p>`;
+      } else {
+        serverStatusElement.innerHTML = `<p>服务器貌似离线了( 可能是在维护吧</p>`;
+      }
+    } catch (error) {
+      serverStatusElement.innerHTML = `<p>服务器貌似离线了( 可能是在维护吧</p>``<p>错误信息:${error.message}</p>`;
+    }
+  });
